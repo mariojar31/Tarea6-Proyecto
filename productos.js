3f4fd92a4755b30d1851fcb6cb7f0413c15d2f5d
Mario Acendra <mariojoacre31@gmail.com>
2024-09-27 23:50:21 -0500

    Actualización Tarea 6 Navegación y adelanto tarea7

--- productos.js
+++ productos.js
@@ -1,40 +1,95 @@
-const divPagos = document.getElementById('producto');
+const divPagos = document.getElementById('pagos');
+const divCafeteria = document.getElementById('cafeteria');
+const divPapeleria = document.getElementById('papeleria');
+const swiperContainer = document.querySelector('swiper-container');
 
+
+const ajustarSwiper = (width)=>{
+  if(width<640){
+    swiperContainer.setAttribute('slides-per-view',1)
+    swiperContainer.setAttribute('space-between',10)
+  }
+  if(width>=640 && width<768){
+    swiperContainer.setAttribute('slides-per-view',2)
+    swiperContainer.setAttribute('space-between',20)
+  }
+  if(width>=768 && width<1000){
+    swiperContainer.setAttribute('slides-per-view',3)
+    swiperContainer.setAttribute('space-between',20)
+  }
+
+  if(width>1000){
+    swiperContainer.setAttribute('slides-per-view',4)
+    swiperContainer.setAttribute('space-between',50)
+  }
+
+
+
+
+
+}
+
+ajustarSwiper(window.screen.width)
+
+addEventListener('resize',()=>{
+  var width = innerWidth;
+
+  ajustarSwiper(width)
+  
+})
 
 const fetchProducts = async()=>{
     const response = await fetch('./db/products.json');
     const data = await response.json();
-    const productos_Pagos = data.productos[0].items
 
-    productos_Pagos.map(item=>{
-        const productoP = document.createElement('div');
-
-        productoP.innerHTML=`<div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
-                        <div class="card mb-3" style="max-width: 540px;">
-                            <div class="row g-0">
-                              <div class="col-md-4">
-                                <img src="#" class="img-fluid rounded-start" alt="...">
-                              </div>
-                              <div class="col-md-8">
-                                <div class="card-body">
-                                  <h5 class="card-title">${item.nombre}</h5>
-                                  <p class="card-text">${item.descripcion}</p>
-                                  
-                                  <p><b>Precio: </b><span>${item.precio}</span></p>
-                                  
-                                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                    </div>
-
-                </div>`
-
-        divPagos.append(productoP)
-
+    data.productos.map(cat=>{
+      if(cat.categoria=='Pagos'){
+        cat.items.map(item=>{
+          const productoP = document.createElement('swiper-slide');
+  
+          productoP.innerHTML=`<div class="item">
+                                <img src="${item.fotografia}">
+                                <div class="title">${item.nombre}</div>
+                                <div class="price">Precio: $${item.precio.toLocaleString('de-DE')}</div>
+                                <button onclick="addToCard(0)">Add To Card</button>
+                              </item>`
+  
+          divPagos.append(productoP)
+  
+      })
+      }else if(cat.categoria=='Cafeteria'){
+        cat.items.map(item=>{
+          const productoP = document.createElement('swiper-slide');
+  
+          productoP.innerHTML=`<div class="item">
+                                <img src="${item.fotografia}">
+                                <div class="title">${item.nombre}</div>
+                                <div class="price">Precio: $${item.precio}</div>
+                                <button onclick="addToCard(0)">Add To Card</button>
+                              </item>`
+  
+          divCafeteria.append(productoP)
+  
+      })
+      }else if(cat.categoria=='Papeleria'){
+        cat.items.map(item=>{
+          const productoP = document.createElement('swiper-slide');
+  
+          productoP.innerHTML=`<div class="item">
+                                <img src="${item.fotografia}">
+                                <div class="title">${item.nombre}</div>
+                                <div class="price">Precio: $${item.precio}</div>
+                                <button onclick="addToCard(0)">Add To Card</button>
+                              </item>`
+  
+          divPapeleria.append(productoP)
+  
+      })
+      }
     })
+    
+
+  
 }
 
 fetchProducts();
