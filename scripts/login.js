f1ef59f22ecd56bcb399a4e428c5654d23c0af1f
mariojar31 <mariojoacre31@gmail.com>
2024-10-11 22:16:30 -0500

    Update Tarea 10

--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,13 @@
-// Obteniendo elementos del DOM
 const formLogin = document.querySelector(".form-login");
 const inputEmail = document.querySelector('.form-login input[type="email"]');
 const inputPass = document.querySelector('.form-login input[type="password"]');
 const alertaErrorLogin = document.querySelector(".alerta-error");
 const alertaExitoLogin = document.querySelector(".alerta-exito");
 
-// Regex para validaciones
 const emailRegex = /^[a-zA-Z0-9]{1,20}([.-_]{1}[a-zA-Z0-9]{1,10})?@[a-zA-Z0-9]{1,10}([-.]{1}[a-zA-Z0-9]{1,20})?\.[a-zA-Z0-9]{2,6}$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\+\-\*\^\/@\{\}\[\]\)\(\.=,%()\?!"#$&\|°])[A-Za-z0-9\+\-\*\^\/@\{\}\[\]\)\(\.=,%()\?!"#$&\|°]{8,10}$/;
 
+// Función para validar un campo
 const validarCampo = (regex, input, mensajeError) => {
     if (regex.test(input.value)) {
         input.classList.remove("input-error");
@@ -21,19 +20,34 @@ const validarCampo = (regex, input, mensajeError) => {
     }
 };
 
-const enviarFormulario = (formulario) => {
+// Función para encriptar la contraseña
+async function unhashPassword(password) {
+    const key = "Qwerty123*"; 
+    const bytes = await CryptoJS.AES.decrypt(password, key);
+    const contraseñaDesencriptada = await bytes.toString(CryptoJS.enc.Utf8);
+    return contraseñaDesencriptada
+}
+
+// Función para enviar el formulario
+const enviarFormulario = async (formulario) => {
     const emailValido = validarCampo(emailRegex, inputEmail, "El correo no es válido.");
     const passValida = validarCampo(passwordRegex, inputPass, "La contraseña debe contener entre 8 y 10 caracteres con mayúsculas, minúsculas, números y caracteres especiales.");
     
     if (emailValido && passValida) {
-        // Comprobamos si el usuario existe en el localStorage
         const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-        const usuarioEncontrado = usuarios.find(usuario => usuario.Email === inputEmail.value && usuario.Contraseña === inputPass.value);
+        const usuarioEncontrado = usuarios.find(usuario => usuario.Email === inputEmail.value);
 
         if (usuarioEncontrado) {
-            alertaExitoLogin.innerText = "Inicio de sesión exitoso.";
-            alertaErrorLogin.innerText = "";
-            // Aquí puedes redirigir al usuario a otra página o realizar cualquier acción adicional
+            const unhashedPassword = await unhashPassword(usuarioEncontrado.Contraseña)
+            console.log(unhashedPassword)
+            if (unhashedPassword === inputPass.value) {
+                alertaExitoLogin.innerText = "Inicio de sesión exitoso.";
+                alertaErrorLogin.innerText = "";
+                window.location.href = 'Admin.html';
+            } else {
+                alertaErrorLogin.innerText = "Credenciales incorrectas. Por favor, verifica tu correo y contraseña.";
+                alertaExitoLogin.innerText = "";
+            }
         } else {
             alertaErrorLogin.innerText = "Credenciales incorrectas. Por favor, verifica tu correo y contraseña.";
             alertaExitoLogin.innerText = "";
--- scripts/register.js
+++ scripts/register.js
@@ -1,172 +1,147 @@
-//Sacar el valor
-const nombreLogin=document.getElementById('nombre_login');
-const telefono= document.getElementById('telefono');
-const email= document.getElementById('email');
-const confirmarContraeña= document.getElementById('confirmarContraeña');
-const contraeña= document.getElementById('contraeña');
-const codigo= document.getElementById('codigo');
+// Obtener elementos del DOM
+const nombreLogin = document.getElementById('nombre_login');
+const telefono = document.getElementById('telefono');
+const email = document.getElementById('email');
+const confirmarContraeña = document.getElementById('confirmarContraeña');
+const contraeña = document.getElementById('contraeña');
+const codigo = document.getElementById('codigo');
 
-//textos almacenar el ERROR
-const mensajeError= document.getElementById('mensajeErrorLogin');
-const nombErrortext= document.getElementById('mensErrornombre');
-const teleErrortext= document.getElementById('mensteleerror');
-const emailErrortext= document.getElementById('mensEmailerror');
-const contrErrortext= document.getElementById('mensErrorcontra');
-const codigErrortext= document.getElementById('menscodierror');
-const conficontraErrortext= document.getElementById('confcontrError');
+// Elementos para mostrar errores
+const mensajeError = document.getElementById('mensajeErrorLogin');
+const nombErrortext = document.getElementById('mensErrornombre');
+const teleErrortext = document.getElementById('mensteleerror');
+const emailErrortext = document.getElementById('mensEmailerror');
+const contrErrortext = document.getElementById('mensErrorcontra');
+const codigErrortext = document.getElementById('menscodierror');
+const conficontraErrortext = document.getElementById('confcontrError');
 
-const formularioLogin= document.getElementById('formularioLogin');
-let objetoLogin;
+const formularioLogin = document.getElementById('formularioLogin');
 
-formularioLogin.addEventListener('submit', (event)=>{
-    event.preventDefault();
-    let bdlogin=JSON.parse(localStorage.getItem('usuarios'))
-        if(teleErrortext.innerText==="" && nombErrortext.innerText==="" && emailErrortext.innerText==="" && contrErrortext.innerText===""&& conficontraErrortext.innerText==="" && codigErrortext.innerText===""){
-        
-            if(bdlogin===null){
-                bdlogin=[];
-                alert("esta nulo")
-             objetoLogin=[
-            {"id":1,"Nombre":nombreLogin.value,"Telefono":telefono.value,"Codigo": codigo.value,"Email":email.value,"Contraseña":contraeña.value}
-                    
-            ]
-            bdlogin.push(objetoLogin)
-            localStorage.setItem('usuarios',JSON.stringify(bdlogin));
-            alert("Usuario creado con éxito")
-        }
-        else{ 
-            
-            alert("ya hay")
-            let arrayNumeros=[];
-
-            bdlogin.forEach(element => {
-               console.log( arrayNumeros.push(element.id));
-               
-            });
-            
-            
-
-           
-        }
-        }
-        
-})
-
-class mensaje{
-
-    constructor(inputss, mensajeobjeto,confirmarContraseña){
-        
-    this.input= inputss;  
-    this.mensaje=mensajeobjeto  
-    this.confirmacioncon=confirmarContraseña;
+// Función para hashear la contraseña usando SHA-256
+async function hashPassword(password) {
+    const key = "Qwerty123*"; // Debes definir una llave secreta para cifrar/desencriptar
+    const contraseñaEncriptada = CryptoJS.AES.encrypt(password, key).toString();
+    console.log
+    return contraseñaEncriptada
 }
-mostrarMensaje(){
-    this.input.addEventListener('input',()=>{
-        
-        let inputActual= this.input.value;
-        if(this.input===nombreLogin){
-            if(!/^[A-Za-z]([\-\._]{1})?[A-Za-z0-9\-\._]{3,19}$/.test(inputActual))
-            {this.mensaje.innerText="Nombre invalido debe tener minimo 4 carcater máximo 20, max un caracter especial (. - _) debe comenzar con letras"}
-            else{ this.mensaje.innerText="";}
+
+// Clase para mostrar mensajes de error
+class Mensaje {
+    constructor(input, mensajeObj, confirmarContraseña) {
+        this.input = input;
+        this.mensaje = mensajeObj;
+        this.confirmacionCon = confirmarContraseña;
+        this.init();
+    }
+
+    init() {
+        this.input.addEventListener('input', () => this.validar());
+    }
+
+    validar() {
+        const inputActual = this.input.value;
+
+        if (this.input === nombreLogin) {
+            this.mensaje.innerText = /^[A-Za-z]([\-\._]{1})?[A-Za-z0-9\-\._]{3,19}$/.test(inputActual)
+                ? ""
+                : "Nombre inválido. Debe tener mínimo 4 caracteres, máximo 20, y comenzar con letras.";
         }
 
-        if(this.input===telefono){
-
-        if(!/^\d{8,10}$/.test(inputActual)){
-            let mensajeNumero= "Ingresa un número valido\n "
-            this.mensaje.innerText = mensajeNumero;
-            this.mensaje.style.color="red"
-                  
-        }else{
-
-            this.mensaje.innerText = "";
-        }
-    }  
-
-    if(this.input===codigo){
-
-        if(!/^\d{8,10}$/.test(inputActual)){
-            let mensajeNumero= "Ingresa un código valido\n "
-            this.mensaje.innerText = mensajeNumero;
-            this.mensaje.style.color="red"
-                  
-        }else{
-
-            this.mensaje.innerText = "";
-        }
-    }  
-    
-    if(this.input===email){
-        if(!/^[a-zA-Z0-9]{1,20}([.-_]{1}[a-zA-Z0-9]{1,10})?@[a-zA-Z0-9]{1,10}([-.]{1}[a-zA-Z0-9]{1,20})?\.[a-zA-Z0-9]{2,6}$/.test(inputActual)){
-
-            let mensajeEmal= "Ingresa un correo valido\n "
-            this.mensaje.innerHTML=mensajeEmal;
-        }else{
-            this.mensaje.innerText="";
-        }
-        }
-        if(this.input===contraeña){
-                
-            if( !/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\+\-\*\^\/@\{\}\[\]\)\(\.=,%()\?!"#$&\|°])[A-Za-z0-9\+\-\*\^\/@\{\}\[\]\)\(\.=,%()\?!"#$&\|°]{8,10}$/.test(inputActual)){
-                
-        let mensaContra="Debe contener 8 digitos entre mayusculas, minusculas, carcateres especiales y números"
-                this.mensaje.innerText=mensaContra;
-            }  
-            else{
-                this.mensaje.innerText="";
-            }     
+        if (this.input === telefono) {
+            this.mensaje.innerText = /^\d{8,10}$/.test(inputActual)
+                ? ""
+                : "Ingresa un número válido (8-10 dígitos).";
         }
 
-        if(this.input===confirmarContraeña){
-                if(this.input.value!==this.confirmacioncon.value){
-
-                        this.mensaje.innerText="las contraseñas no son iguales"
-
-                }
-                else{
-                    this.mensaje.innerText="";
-                }
+        if (this.input === codigo) {
+            this.mensaje.innerText = /^\d{8,10}$/.test(inputActual)
+                ? ""
+                : "Ingresa un código válido (8-10 dígitos).";
         }
-       
-    });
-   
-};
-};
 
-let array=[];
+        if (this.input === email) {
+            this.mensaje.innerText = /^[a-zA-Z0-9]{1,20}([.-_]{1}[a-zA-Z0-9]{1,10})?@[a-zA-Z0-9]{1,10}([-.]{1}[a-zA-Z0-9]{1,20})?\.[a-zA-Z0-9]{2,6}$/.test(inputActual)
+                ? ""
+                : "Ingresa un correo válido.";
+        }
 
-let tele= new mensaje(telefono,teleErrortext);
-tele.mostrarMensaje();
+        if (this.input === contraeña) {
+            this.mensaje.innerText = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\+\-\*\^\/@\{\}\[\]\)\(\.=,%()\?!"#$&\|°]).{8,10}$/.test(inputActual)
+                ? ""
+                : "La contraseña debe contener 8 dígitos, mayúsculas, minúsculas, caracteres especiales y números.";
+        }
 
-let emails= new mensaje(email,emailErrortext);
-emails.mostrarMensaje();
+        if (this.input === confirmarContraeña) {
+            this.mensaje.innerText = this.input.value === this.confirmacionCon.value
+                ? ""
+                : "Las contraseñas no son iguales.";
+        }
+    }
+}
 
-let contraseñas= new mensaje(contraeña,contrErrortext);
-contraseñas.mostrarMensaje();
+// Manejo del envío del formulario
+formularioLogin.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-let cofirContras= new mensaje(confirmarContraeña,conficontraErrortext,contraeña);
-cofirContras.mostrarMensaje();
+    const bdlogin = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const errorMessages = [
+        teleErrortext.innerText,
+        nombErrortext.innerText,
+        emailErrortext.innerText,
+        contrErrortext.innerText,
+        conficontraErrortext.innerText,
+        codigErrortext.innerText
+    ];
 
-let nombreLog= new mensaje(nombreLogin,nombErrortext);
-nombreLog.mostrarMensaje();
+    if (errorMessages.every(msg => msg === "")) {
+        const existeCorreo = bdlogin.some(ele => ele.Email === email.value);
 
-let codilog= new mensaje(codigo,codigErrortext)
-codilog.mostrarMensaje();
+        if (existeCorreo) {
+            alert("El correo ya fue creado.");
+            return;
+        }
 
+        const IdLogin = bdlogin.length === 0 ? 1 : Math.max(...bdlogin.map(element => element.id)) + 1;
+        const contraseñaEncriptada = await hashPassword(contraeña.value);
 
+        const objetoLogin = {
+            id: IdLogin,
+            Rol: "Estudiante",
+            Nombre: nombreLogin.value,
+            Telefono: telefono.value,
+            Codigo: codigo.value,
+            Email: email.value,
+            Contraseña: contraseñaEncriptada
+        };
 
-const btnSignIn = document.getElementById("sign-in"),
-      btnSignUp = document.getElementById("sign-up"),
-      containerFormRegister = document.querySelector(".register"),
-      containerFormLogin = document.querySelector(".login");
+        bdlogin.push(objetoLogin);
+        localStorage.setItem('usuarios', JSON.stringify(bdlogin));
+        alert("Usuario creado con éxito.");
+    }
+});
 
-btnSignIn.addEventListener("click", e => {
+// Inicialización de los mensajes de error
+const mensajes = [
+    new Mensaje(nombreLogin, nombErrortext),
+    new Mensaje(telefono, teleErrortext),
+    new Mensaje(email, emailErrortext),
+    new Mensaje(contraeña, contrErrortext),
+    new Mensaje(confirmarContraeña, conficontraErrortext, contraeña),
+    new Mensaje(codigo, codigErrortext)
+];
+
+// Manejo de inicio y registro
+const btnSignIn = document.getElementById("sign-in");
+const btnSignUp = document.getElementById("sign-up");
+const containerFormRegister = document.querySelector(".register");
+const containerFormLogin = document.querySelector(".login");
+
+btnSignIn.addEventListener("click", () => {
     containerFormRegister.classList.add("hide");
-    containerFormLogin.classList.remove("hide")
-})
+    containerFormLogin.classList.remove("hide");
+});
 
-
-btnSignUp.addEventListener("click", e => {
+btnSignUp.addEventListener("click", () => {
     containerFormLogin.classList.add("hide");
-    containerFormRegister.classList.remove("hide")
-})
+    containerFormRegister.classList.remove("hide");
+});
